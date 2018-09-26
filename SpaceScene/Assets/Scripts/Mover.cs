9d86464bce6256714ebf3d41155d7dae324953d2
cmorin2 <cmorin2@my.centennialcollege.ca>
2018-09-26 12:22:19 -0400

    Player Controller

---
+++ SpaceScene/Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+    //Variable Declarations
+    public float speed;
+
+    private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = transform.right * speed;
+	}
+}
---
+++ SpaceScene/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    //Variable Declarations
+    public float speed = 8.0f;
+    public float xMin, xMax, yMin, yMax;
+
+    private Rigidbody2D rBody; 
+    //Declare Rigidbody as private, call once in start.
+    void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Fixed framerate update, used for physics
+    private void FixedUpdate()
+    {
+        //Get input
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horz, vert);
+        //Player movement
+        rBody.velocity = movement * speed;
+        //restrict player movement
+        rBody.position = new Vector2(
+            Mathf.Clamp(rBody.position.x, xMin, xMax),
+            Mathf.Clamp(rBody.position.y, yMin, yMax)
+            );
+        //Debug.Log("x: " + horz +", y: " + vert);
+    }
+}
