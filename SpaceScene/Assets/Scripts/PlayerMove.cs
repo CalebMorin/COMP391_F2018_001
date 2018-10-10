66f75d0260ae1d7990d7230262e62d188eca8113
cmorin2 <301012814@cencol.ca>
2018-10-10 12:23:46 -0400

    asteroid and colliders

--- SpaceScene/Assets/Scripts/PlayerMove.cs
+++ SpaceScene/Assets/Scripts/PlayerMove.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
 public class PlayerMove : MonoBehaviour {
 
     //Variable Declarations
     public float speed = 8.0f;
-    public float xMin, xMax, yMin, yMax;
-
-    private Rigidbody2D rBody; 
+    public Boundary boundary;
+    //public float xMin, xMax, yMin, yMax;
+    public GameObject PlayerLaser;
+    public GameObject topLaserSpawn;
+    public GameObject botLaserSpawn;
+    public float fireRate = 0.5f;
+    private Rigidbody2D rBody;
+    private float myTime = 0.0f;
     //Declare Rigidbody as private, call once in start.
     void Start () {
         rBody = GetComponent<Rigidbody2D>();
@@ -16,7 +26,14 @@ public class PlayerMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //Debug.Log(Time.deltaTime);
+        myTime += Time.deltaTime;
+        if (Input.GetButton("Fire1") && myTime > fireRate)
+        {
+            Instantiate(PlayerLaser, topLaserSpawn.transform.position, topLaserSpawn.transform.rotation);
+            Instantiate(PlayerLaser, botLaserSpawn.transform.position, botLaserSpawn.transform.rotation);
+            myTime = 0.0f;
+        }
 	}
 
     // Fixed framerate update, used for physics
@@ -30,8 +47,8 @@ public class PlayerMove : MonoBehaviour {
         rBody.velocity = movement * speed;
         //restrict player movement
         rBody.position = new Vector2(
-            Mathf.Clamp(rBody.position.x, xMin, xMax),
-            Mathf.Clamp(rBody.position.y, yMin, yMax)
+            Mathf.Clamp(rBody.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rBody.position.y, boundary.yMin, boundary.yMax)
             );
         //Debug.Log("x: " + horz +", y: " + vert);
     }
