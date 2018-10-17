4746d6188d132dd6d105cfc463a7e6fddd243d02
CalebMorin <cmorin2@my.centennialcollege.ca>
2018-10-17 12:24:26 -0400

    added explosion animation

---
+++ SpaceScene/Assets/Scripts/asteroidmover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroidmover : MonoBehaviour {
+    public float speed;
+    public float tumble;
+    private Rigidbody2D rBody;
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = transform.right * speed;
+        rBody.angularVelocity = Random.value * tumble;
+	}
+}
